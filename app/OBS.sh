13fd1dfa68f7ffb62e833f2fafe19dc22a6c3c2d
GoldenUpwinds <YC_JIN@outlook.com>
2023-08-28 15:23:50 +0800

    obs docker

---
+++ app/OBS.sh
@@ -0,0 +1,6 @@
+#！/bin/bash
+
+sudo apt -y install ffmpeg
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install -y obs-studio
---
+++ app/notion.sh
@@ -0,0 +1,5 @@
+#！/bin/bash
+
+echo "deb [trusted=yes] https://apt.fury.io/notion-repackaged/ /" | sudo tee /etc/apt/sources.list.d/notion-repackaged.list
+sudo apt update
+sudo apt install notion-app-enhanced
